4d9f9cab71cb4ab6dcace685edd0a86b5340e1ed
Praful <prafulpatekar23@gmail.com>
2023-05-09 00:30:51 +0530

    Till 35 lecture

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!jdjds</h2>
+      <p>This is cool!</p>
+      <ExpenseItem/>
     </div>
   );
 }
