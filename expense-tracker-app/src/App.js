cda2d5a427af143b1f0c7949793a013cc37cb2f0
Praful <prafulpatekar23@gmail.com>
2023-05-14 13:55:31 +0530

    Section 3 ended

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -1,11 +1,37 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = ()=> {
+  const expenseArray = [
+    {
+      id: "e1",
+      title: "Room Rent",
+      amount: 3500,
+      date: new Date(2023, 4, 20),
+    },
+    {
+      id: "e2",
+      title: "Grocery",
+      amount: 150,
+      date: new Date(2023, 4, 10),
+    },
+    {
+      id: "e3",
+      title: "Food & Drinks",
+      amount: 311,
+      date: new Date(2023, 4, 13),
+    },
+    {
+      id: "e4",
+      title: "Other",
+      amount: 200,
+      date: new Date(2023, 4, 21),
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!jdjds</h2>
-      <p>This is cool!</p>
-      <ExpenseItem/>
+      <h2>Let's get started!</h2>
+      <p>Praful.Coder is cool!</p>
+      <Expenses expenses={expenseArray}/>
     </div>
   );
 }
